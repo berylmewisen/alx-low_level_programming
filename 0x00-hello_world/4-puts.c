ab8d73162d0d82e2adee43dcb247641182d798ae
Mewisen <bmewisen@gmail.com>
2023-02-16 09:56:50 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,8 @@
  */
 int main(void)
 {
-	const char *y = "Programming is like building a multilingual puzzle";
-	puts(y);
-
-	return (0);
-}	
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
 
 
