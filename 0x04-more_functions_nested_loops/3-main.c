2da8105b09d450bd10988322598adcba77c55141
Mewisen <bmewisen@gmail.com>
2023-02-23 00:40:23 -0800

    3main

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks for checks for a digit (0 through 9).
+ *
+ * Return: the numbers since 0 up to 9
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 0; c++)
+	{
+	_putchar(c + '0');
+	}
+	_putchar('\n');
+}
